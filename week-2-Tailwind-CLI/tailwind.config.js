02e5024c230f864f5efe48ca52207dc8dadcdedf
JimmyWu <gmwu185@gmail.com>
2022-12-29 17:58:24 +0800

    擴充客製 colors 的三種方式 (單一色、100-900 同色系列色碼、明暗色系)

--- week-2-Tailwind-CLI/tailwind.config.js
+++ week-2-Tailwind-CLI/tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    /* theme 下設定 colors 客製化，會令 Tailwind 原先的色彩失效 */
+    // colors: {
+    //   lightBlue: '#1fb6ff',
+    // },
+    extend: {
+      colors: {
+        /* 寫法一 */
+        lightBlue: '#1fb6ff',
+        /* 寫法二 */
+        'tahiti': {
+          100: '#cffafe',
+          200: '#a5f3fc',
+          300: '#67e8f9',
+          400: '#22d3ee',
+          500: '#06b6d4',
+          600: '#0891b2',
+          700: '#0e7490',
+          800: '#155e75',
+          900: '#164e63',
+        },
+        /* 寫法三 */
+        'tahitiTwo': {
+          light: '#67e8f9',
+          DEFAULT: '#06b6d4',
+          dark: '#0e7490',
+        }
+      },
+    },
   },
   plugins: [],
-}
+};
