6aab354317923b5f4908bd9be426c1dacda81523
JimmyWu <gmwu185@gmail.com>
2022-12-30 23:31:54 +0800

    1. 更換使用自定主題色  2. 表單樣式調整

--- week-2-Tailwind-plugin-form/tailwind.config.js
+++ week-2-Tailwind-plugin-form/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#AA06010D',
+          DEFAULT: '#AA0601',
+          dark: '#650300',
+        },
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
